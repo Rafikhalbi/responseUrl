77a361540d9d32a7e07474916c7684cb86e626cf
RAFI KHALBI <89525492+rafikhalbi@users.noreply.github.com>
2022-04-03 05:49:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def cek(url):
     except (KeyError,IOError):
         exit("! URL Tidak Di Temukan")
     print(f"\n* Jika Status Code: <Response [200]> Anda Dapat Lansung Scrap Website {url} tanpa Headers ")
-    print("\n* Jika Status Code: <Response [404]> Tandanya %s Harus memasukan Headers\n* Contoh:\n  headers= {'user-agent':'masukan User agent Anda'}\n  req = requests.get('%s', headers = headers)\n* Coba Print Lagi: print(req)\n* Jika <Response [200]> Berarti Website %s Sudah Bisa Di scrap"%(url,url,url)) 
+    print("\n* Jika Status Code: ±<Response [404]> Tandanya %s Harus memasukan Headers\n* Contoh:\n  headers= {'user-agent':'masukan User agent Anda'}\n  req = requests.get('%s', headers = headers)\n* Coba Print Lagi: print(req)\n* Jika <Response [200]> Berarti Website %s Sudah Bisa Di scrap"%(url,url,url)) 
 
 if __name__ == '__main__':
     cek(url)
